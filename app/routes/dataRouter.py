491ed11065fab1af31b4c24ca8971796eb58dbaa
Shahilcoder <shahilcoder@gmail.com>
2023-12-07 22:24:56 +0530

    coach put and delete

--- app/routes/dataRouter.py
+++ app/routes/dataRouter.py
@@ -1,12 +1,23 @@
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from flask_jwt_extended import jwt_required
+
+from app.db import db
 
 dataRouter = Blueprint("data", __name__, url_prefix="/data")
 
 @dataRouter.route("/coach/update", methods=("PUT",))
+@jwt_required()
 def update_coach():
-    return "put"
+    payload = request.json
 
-@dataRouter.route("/coach/delete", methods=("DELETE",))
-def delete_coach():
-    return "delete"
+    coach_coll = db.coaches
+    coach_coll.update_one({'coach_id': payload['coach_id']}, {'$set': payload})
+    return jsonify(msg="Coach Updated"), 200
+
+@dataRouter.route("/coach/delete/<coach_id>", methods=("DELETE",))
+@jwt_required()
+def delete_coach(coach_id):
+    coach_coll = db.coaches
+    coach_coll.delete_one({'coach_id': coach_id})
+
+    return jsonify(msg="Coach Deleted"), 200
